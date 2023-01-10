26b73ad8826becde96b123d14ca0eb312b759eed
pasly0920 <inwoo920621@gmail.com>
2023-01-10 22:52:24 +0900

    feat: 기본적 domain model 추가

---
+++ src/main/java/jpabook/japshop/domain/OrderItem.java
@@ -0,0 +1,32 @@
+package jpabook.japshop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import jpabook.japshop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
