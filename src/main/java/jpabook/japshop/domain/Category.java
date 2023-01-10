4d233bce4a418bdf8c4b88527de3d9f67098fbeb
pasly0920 <inwoo920621@gmail.com>
2023-01-11 00:03:30 +0900

    feat: XXXToOne 지연 로딩 일괄 추가

--- src/main/java/jpabook/japshop/domain/Category.java
+++ src/main/java/jpabook/japshop/domain/Category.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -34,7 +35,7 @@ public class Category {
     )
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
