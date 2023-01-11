e3ce05b08906346e7fd5ab30a9ef95a8e870d631
pasly0920 <inwoo920621@gmail.com>
2023-01-11 11:59:15 +0900

    feat: Member Repository, Service 구축

---
+++ src/main/java/jpabook/japshop/service/MemberService.java
@@ -0,0 +1,38 @@
+package jpabook.japshop.service;
+
+import java.util.List;
+import jpabook.japshop.domain.Member;
+import jpabook.japshop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
