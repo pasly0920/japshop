5fc22cd680a9571956b138a065af56e0105a01f2
pasly0920 <inwoo920621@gmail.com>
2023-01-11 13:45:19 +0900

    feat: item stock 수량 부족 exception 추가

---
+++ src/main/java/jpabook/japshop/exception/NotEnoughStockException.java
@@ -0,0 +1,21 @@
+package jpabook.japshop.exception;
+
+public class NotEnoughStockException extends Throwable {
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+}
