1364d55746adc0b4cb2ed8fdbac0a9aa0d064d7f
pasly0920 <inwoo920621@gmail.com>
2023-01-11 15:30:33 +0900

    feat: Order 생성, 비즈니스 로직, 조회 로직 메서드 추가

--- src/main/java/jpabook/japshop/domain/Order.java
+++ src/main/java/jpabook/japshop/domain/Order.java
@@ -46,6 +46,39 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        if (delivery.getDeliveryStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
+
     //==연관관계 메서드==//
     public void setMember(Member member) {
         this.member = member;
