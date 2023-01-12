a56caf484d0795f0e67bf33e4bc3c07779e6c92b
pasly0920 <inwoo920621@gmail.com>
2023-01-12 11:45:41 +0900

    feat: 반환 타입 맞춰주기

--- src/main/java/jpabook/japshop/repository/OrderRepository.java
+++ src/main/java/jpabook/japshop/repository/OrderRepository.java
@@ -21,7 +21,7 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        em.createQuery("select o from Order o join o.member m"
+        return em.createQuery("select o from Order o join o.member m"
                         + " where o.status = :status"
                         + " and m.name like :name", Order.class)
                 .setParameter("status", orderSearch.getOrderStatus())
